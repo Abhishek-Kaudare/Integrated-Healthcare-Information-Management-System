19ff63ce890db3ca89a48aad81efe6199fafeb99
Rajpreet16 <rajpreet24033@gmail.com>
2019-10-11 22:04:21 +0530

    UI Done

--- resources/views/demo/dashboard.blade.php
+++ resources/views/demo/dashboard.blade.php
@@ -6,193 +6,68 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/extra-libs/multicheck/multicheck.css') }}">
 <link href="{{ asset('admin/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css') }}" rel="stylesheet">
 <link href="{{ asset('admin/dist/css/style.min.css') }}" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/libs/select2/dist/css/select2.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/libs/jquery-minicolors/jquery.minicolors.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/libs/quill/dist/quill.snow.css') }}">
+<link href="{{ asset('admin/dist/css/style.min.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
-<div class="row">
-  <!-- Column -->
-  <div class="col-md-6 col-lg-4 col-xlg-3">
-    <div class="card card-hover">
-      <div class="box bg-cyan text-center">
-        <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
-        <h6 class="text-white">My Dashboard</h6>
-      </div>
-    </div>
-  </div>
-  <!-- Column -->
-  <div class="col-md-6 col-lg-4 col-xlg-3">
-    <div class="card card-hover">
-      <div class="box bg-success text-center">
-        <h1 class="font-light text-white"><i class="mdi mdi-chart-areaspline"></i></h1>
-        <h6 class="text-white">Doctors</h6>
-      </div>
-    </div>
-  </div>
-  <!-- Column -->
-  <div class="col-md-6 col-lg-4 col-xlg-3">
-    <div class="card card-hover">
-      <div class="box bg-warning text-center">
-        <h1 class="font-light text-white"><i class="mdi mdi-collage"></i></h1>
-        <h6 class="text-white">Patients</h6>
-      </div>
-    </div>
-  </div>
-  <!-- Column -->
-  <!-- Column -->
-  <div class="col-md-6 col-lg-4 col-xlg-3">
-    <div class="card card-hover">
-      <div class="box bg-danger text-center">
-        <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
-        <h6 class="text-white">Bed Availability</h6>
-      </div>
-    </div>
-  </div>
-  <!-- Column -->
-  <div class="col-md-6 col-lg-4 col-xlg-3">
-    <div class="card card-hover">
-      <div class="box bg-info text-center">
-        <h1 class="font-light text-white"><i class="mdi mdi-chart-areaspline"></i></h1>
-        <h6 class="text-white">Other Facilities</h6>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title"></h5>
-    <div class="table-responsive">
-      <table id="zero_config" class="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Tiger Nixon</td>
-            <td>System Architect</td>
-            <td>Edinburgh</td>
-            <td>61</td>
-            <td>2011/04/25</td>
-            <td>$320,800</td>
-          </tr>
-          <tr>
-            <td>Garrett Winters</td>
-            <td>Accountant</td>
-            <td>Tokyo</td>
-            <td>63</td>
-            <td>2011/07/25</td>
-            <td>$170,750</td>
-          </tr>
-          <tr>
-            <td>Ashton Cox</td>
-            <td>Junior Technical Author</td>
-            <td>San Francisco</td>
-            <td>66</td>
-            <td>2009/01/12</td>
-            <td>$86,000</td>
-          </tr>
-          <tr>
-            <td>Cedric Kelly</td>
-            <td>Senior Javascript Developer</td>
-            <td>Edinburgh</td>
-            <td>22</td>
-            <td>2012/03/29</td>
-            <td>$433,060</td>
-          </tr>
-          <tr>
-            <td>Airi Satou</td>
-            <td>Accountant</td>
-            <td>Tokyo</td>
-            <td>33</td>
-            <td>2008/11/28</td>
-            <td>$162,700</td>
-          </tr>
-          <tr>
-            <td>Brielle Williamson</td>
-            <td>Integration Specialist</td>
-            <td>New York</td>
-            <td>61</td>
-            <td>2012/12/02</td>
-            <td>$372,000</td>
-          </tr>
-          <tr>
-            <td>Herrod Chandler</td>
-            <td>Sales Assistant</td>
-            <td>San Francisco</td>
-            <td>59</td>
-            <td>2012/08/06</td>
-            <td>$137,500</td>
-          </tr>
-          <tr>
-            <td>Rhona Davidson</td>
-            <td>Integration Specialist</td>
-            <td>Tokyo</td>
-            <td>55</td>
-            <td>2010/10/14</td>
-            <td>$327,900</td>
-          </tr>
-          <tr>
-            <td>Colleen Hurst</td>
-            <td>Javascript Developer</td>
-            <td>San Francisco</td>
-            <td>39</td>
-            <td>2009/09/15</td>
-            <td>$205,500</td>
-          </tr>
-          <tr>
-            <td>Sonya Frost</td>
-            <td>Software Engineer</td>
-            <td>Edinburgh</td>
-            <td>23</td>
-            <td>2008/12/13</td>
-            <td>$103,600</td>
-          </tr>
-          <tr>
-            <td>Jena Gaines</td>
-            <td>Office Manager</td>
-            <td>London</td>
-            <td>30</td>
-            <td>2008/12/19</td>
-            <td>$90,560</td>
-          </tr>
-          <tr>
-            <td>Quinn Flynn</td>
-            <td>Support Lead</td>
-            <td>Edinburgh</td>
-            <td>22</td>
-            <td>2013/03/03</td>
-            <td>$342,000</td>
-          </tr>
-          <tr>
-            <td>Charde Marshall</td>
-            <td>Regional Director</td>
-            <td>San Francisco</td>
-            <td>36</td>
-            <td>2008/10/16</td>
-            <td>$470,600</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
 
-  </div>
-</div>
+        <div class="card">
+            <form class="form-horizontal">
+                <div class="card-body">
+                        <h2 class="card-title"style="margin-left:45%;color:#2255a4;font-weight:bold;font-family:Sans">Register Campaign</h2></br>
+                    <div class="form-group row">
+                        <label for="fname" class="col-sm-3 text-right control-label col-form-label">Name</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="cname" placeholder="Enter Campaign Name">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Address</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="address" placeholder="Enter full address">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Latitude</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" id="lat" placeholder="Enter Latitude">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="email1" class="col-sm-3 text-right control-label col-form-label">Longitude</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="long" placeholder="Enter longitude">
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <label class="m-t-15"style="margin-left:19%">Start Date</label>
+                        <input type="text" style="margin-left:19px" class="form-control" id="datepicker-autoclose" placeholder="mm/dd/yyyy">
+                        <div class="input-group-append">
+                            <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="input-group">
+                            <label class="m-t-15"style="margin-left:19%">End Date</label>
+                            <input type="text" style="margin-left:19px" class="form-control" id="datepicker-autoclose1" placeholder="mm/dd/yyyy">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                            </div>
+                        </div>
+               
+       
+        <div class="border-top">
+            <div class="card-body">
+                <button type="button" Style="margin-left:50%;color: white;font-weight:bold;border-radius: 55px;padding: 12px;" class="btn btn-info">Register</button>
+            </div>
+        </div>
+    </form>
+    
+    </div>
 @endsection
 
 @section('script')
@@ -219,5 +94,78 @@
          ****************************************/
         $('#zero_config').DataTable();
 </script>
+<script src="{{ asset('admin/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{ asset('admin/assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="{{ asset('admin/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/extra-libs/sparkline/sparkline.js') }}"></script>
+    <!--Wave Effects -->
+    <script src="{{ asset('admin/dist/js/waves.js') }}"></script>
+    <!--Menu sidebar -->
+    <script src="{{ asset('admin/dist/js/sidebarmenu.js') }}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{ asset('admin/dist/js/custom.min.js') }}"></script>
+    <!-- This Page JS -->
+    <script src="{{ asset('admin/assets/libs/inputmask/dist/min/jquery.inputmask.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/dist/js/pages/mask/mask.init.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/select2/dist/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/select2/dist/js/select2.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/jquery-asColor/dist/jquery-asColor.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/jquery-asGradient/dist/jquery-asGradient.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/jquery-asColorPicker/dist/jquery-asColorPicker.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/jquery-minicolors/jquery.minicolors.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/libs/quill/dist/quill.min.js') }}"></script>
+    <script>
+        //***********************************//
+        // For select 2
+        //***********************************//
+        $(".select2").select2();
 
+        /*colorpicker*/
+        $('.demo').each(function() {
+        //
+        // Dear reader, it's actually very easy to initialize MiniColors. For example:
+        //
+        //  $(selector).minicolors();
+        //
+        // The way I've done it below is just for the demo, so don't get confused
+        // by it. Also, data- attributes aren't supported at this time...they're
+        // only used for this demo.
+        //
+        $(this).minicolors({
+                control: $(this).attr('data-control') || 'hue',
+                position: $(this).attr('data-position') || 'bottom left',
+
+                change: function(value, opacity) {
+                    if (!value) return;
+                    if (opacity) value += ', ' + opacity;
+                    if (typeof console === 'object') {
+                        console.log(value);
+                    }
+                },
+                theme: 'bootstrap'
+            });
+
+        });
+        /*datwpicker*/
+        jQuery('.mydatepicker').datepicker();
+        jQuery('#datepicker-autoclose').datepicker({
+            autoclose: true,
+            todayHighlight: true
+        });
+        var quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+        jQuery('.mydatepicker').datepicker();
+        jQuery('#datepicker-autoclose1').datepicker({
+            autoclose: true,
+            todayHighlight: true
+        });
+        var quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+    </script>
 @endsection
